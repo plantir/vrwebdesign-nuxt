8b8689fcdcf3b478a8edacbec4346698a5f228b6
arminkheirkhahan <arminkheirkhahan@gmail.com>
2019-06-24 20:28:39 +0430

    fix v-form-generator

--- modules/nuxt-form-generator/components/v-form-generator.vue
+++ modules/nuxt-form-generator/components/v-form-generator.vue
@@ -1,12 +1,77 @@
-<style lang="sass" scoped>
+<style lang="scss">
+.form-section {
+  h3 {
+    display: block;
+    font-size: 1.5rem;
+    font-weight: 500;
+    padding: 0;
+    margin: 1rem 0 1rem 0;
+    color: #464457;
+  }
+  .form-group {
+    display: flex;
+    > label {
+      flex: 0 0 25%;
+      color: #646c9a;
+      padding: 12px;
+    }
+    .v-input {
+      font-size: 13px;
+      &.v-input--is-focused {
+        .v-input__slot {
+          border-color: $primary-color !important;
+        }
+      }
+      .v-input__slot {
+        min-height: 38px;
+        border-width: 1px !important;
+        border-color: #e2e5ec !important;
+        align-items: center;
+      }
+      .v-input__prepend-inner {
+        margin-top: 6px;
+      }
+      .v-input__append-inner {
+        margin-top: 6px;
+      }
 
+      .v-label {
+        top: 7px;
+        font-size: 13px;
+      }
+      input {
+        margin-top: 0;
+      }
+    }
+    .v-input--selection-controls {
+      margin-top: 0;
+      padding-top: 0;
+      .v-label {
+        top: 0;
+      }
+    }
+    .v-messages {
+      min-height: 18px;
+    }
+    .v-text-field__suffix {
+      background: #f5f5f5;
+      height: 38px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-left: -12px;
+      min-width: 40px;
+      border-radius: 4px 0 0 4px;
+    }
+  }
+}
 </style>
 
 <template>
   <section>
-    <div class="form-section" v-for="(item, sectionIndex) in formData" :key="sectionIndex">
-      <h3 v-if="item.title">{{item.title}}</h3>
-      <template v-for="(field, fieldIndex) in item.rows">
+    <div class="form-section" v-for="(row, sectionIndex) in formData" :key="sectionIndex">
+      <h3 v-if="row.title">{{row.title}}</h3>
+      <template v-for="(field, fieldIndex) in row.rows">
         <component
           v-if="field.model.includes('.')"
           :key="fieldIndex"
@@ -31,9 +96,7 @@
     </div>
   </section>
 </template>
-<script>
-import { watch } from 'chokidar'
-
+<script >
 import FormControlls from './form-controlls/index'
 export default {
   components: FormControlls,
@@ -47,12 +110,25 @@ export default {
   },
   data() {
     return {
-      item: this.value
+      item: { ...this.value }
     }
   },
+
   watch: {
-    item: function() {
-      this.$emit('input', this.item)
+    value: {
+      handler: function(value, oldValue) {
+        if (JSON.stringify(value) == JSON.stringify(oldValue)) {
+          return
+        }
+        this.item = { ...value }
+      },
+      deep: true
+    },
+    item: {
+      handler: function() {
+        this.$emit('input', this.item)
+      },
+      deep: true
     }
   }
 }
