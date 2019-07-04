0317407094bb3102c24b6a3b799027139197d8e6
arminkheirkhahan <arminkheirkhahan@gmail.com>
2019-07-04 11:11:32 +0430

    declare module for type in form generator

--- modules/nuxt-form-generator/types/index.d.ts
+++ modules/nuxt-form-generator/types/index.d.ts
@@ -1,20 +1,22 @@
-interface VRFormInput {
-  title?: string
-  type?:
-    | 'checkbox'
-    | 'currency'
-    | 'datePicker'
-    | 'fileUpload'
-    | 'radio'
-    | 'select'
-    | 'textArea'
-    | 'textField'
-  model: string
-  validation?: string | any
-  ref?: string
-  disabled?: string
-  readonly?: string
-  component?: string
-  label?: string
+declare module 'nuxt-form-generator' {
+  interface VRFormInput {
+    title?: string
+    type?:
+      | 'checkbox'
+      | 'currency'
+      | 'datePicker'
+      | 'fileUpload'
+      | 'radio'
+      | 'select'
+      | 'textArea'
+      | 'textField'
+    model: string
+    validation?: string | any
+    ref?: string
+    disabled?: string
+    readonly?: string
+    component?: string
+    label?: string
+  }
+  type VRFormData = VRFormInput[]
 }
-type VRFormData = VRFormInput[]
