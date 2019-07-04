81a3497185e549c1a91310642dd769dd28f3c358
arminkheirkhahan <arminkheirkhahan@gmail.com>
2019-07-04 10:24:03 +0430

    add type to form generator

---
+++ modules/nuxt-form-generator/types/index.d.ts
@@ -0,0 +1,11 @@
+interface VRFormInput {
+  title: string
+  model: string
+  validation: string | any
+  ref: string
+  disabled: string
+  readonly: string
+  component: string
+  label: string
+}
+type VRFormData = VRFormInput[]
