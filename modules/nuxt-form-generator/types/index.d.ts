a861f0dbc9818999315c0ae60520783235a9230a
arminkheirkhahan <arminkheirkhahan@gmail.com>
2019-07-04 12:07:56 +0430

    export interface in form generator type

--- modules/nuxt-form-generator/types/index.d.ts
+++ modules/nuxt-form-generator/types/index.d.ts
@@ -1,22 +1,20 @@
-declare module 'nuxt-form-generator' {
-  interface VRFormInput {
-    title?: string
-    type?:
-      | 'checkbox'
-      | 'currency'
-      | 'datePicker'
-      | 'fileUpload'
-      | 'radio'
-      | 'select'
-      | 'textArea'
-      | 'textField'
-    model: string
-    validation?: string | any
-    ref?: string
-    disabled?: string
-    readonly?: string
-    component?: string
-    label?: string
-  }
-  type VRFormData = VRFormInput[]
+export interface VRFormInput {
+  title?: string
+  type?:
+    | 'checkbox'
+    | 'currency'
+    | 'datePicker'
+    | 'fileUpload'
+    | 'radio'
+    | 'select'
+    | 'textArea'
+    | 'textField'
+  model: string
+  validation?: string | any
+  ref?: string
+  disabled?: string
+  readonly?: string
+  component?: string
+  label?: string
 }
+export type VRFormData = VRFormInput[]
