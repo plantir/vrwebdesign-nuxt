879be2627f2ae39fdfa577775cd93a533b427a78
nmomenyan <momenyan@gmail.com>
2019-07-14 18:52:14 +0430

    checkbox

--- modules/nuxt-form-generator/components/form-controlls/checkbox.vue
+++ modules/nuxt-form-generator/components/form-controlls/checkbox.vue
@@ -1,3 +1,31 @@
 <template>
-  <div>checkbox</div>
+  <div class="form-group">
+    <v-checkbox
+      v-bind="$attrs"
+      v-on="$listeners"
+      outline
+      :browser-autocomplete="field.browserAutocomplete || 'off'"
+      :error-messages="errorMessages"
+      :placeholder="placeholder"
+      :suffix="field.suffix"
+    ></v-checkbox>
+    <label>{{field.label}}</label>
+  </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+import mixins from './mixins'
+export default Vue.extend({
+  mixins: [mixins]
+})
+</script>
+
+<style lang="scss" scoped>
+.form-section {
+  .form-group {
+    > label {
+      flex: 0 0 95%;
+    }
+  }
+}
+</style>
