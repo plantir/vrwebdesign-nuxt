50e725c291087159935350df81fcbc18bec55c7b
nmomenyan <momenyan@gmail.com>
2019-07-17 16:11:59 +0430

    value as int

--- modules/nuxt-enums/lib/module.js
+++ modules/nuxt-enums/lib/module.js
@@ -10,16 +10,24 @@ function axiosModule(_moduleOptions) {
   var enum_path = path.join(this.options.rootDir, 'enums')
   if (fs.existsSync(enum_path)) {
     // Do something
-    fs.readdirSync(enum_path).forEach(function(file) {
+    fs.readdirSync(enum_path).forEach(function (file) {
       if (file.includes('.ts') || file.includes('.js')) {
         var file_path = path.join(enum_path, file)
         delete require.cache[require.resolve(file_path)]
         //  to have this service in enums
         const file_name = file.replace(/\.(js|ts)/, '')
         enums[file_name] = _interopDefault(require(file_path))
-        let convertToSelect = Object.keys(enums[file_name]).map(key => {
-          return { text: enums[file_name][key], value: key }
-        })
+
+        let convertToSelect = []
+        for (let key in enums[file_name]) {
+          try {
+            let new_key = parseInt(key)
+            key = new_key
+          } catch (error) {
+
+          }
+          convertToSelect.push({ text: enums[file_name][key], value: key })
+        }
         enums[file_name]['toSelect'] = convertToSelect
       }
     })
