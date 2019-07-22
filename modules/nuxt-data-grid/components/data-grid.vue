5c228ba9d7507f71cfae42c48301ccb58ebbdb25
arminkheirkhahan <arminkheirkhahan@gmail.com>
2019-07-22 22:43:40 +0430

    add outline to button

--- modules/nuxt-data-grid/components/data-grid.vue
+++ modules/nuxt-data-grid/components/data-grid.vue
@@ -223,7 +223,7 @@
             <span>تازه کردن اطلاعات</span>
           </v-tooltip>
 
-          <v-btn v-if="withAdd" @click="_add" class="add-new" color="primary" round>
+          <v-btn v-if="withAdd" @click="_add" class="add-new" color="primary" round outline>
             <v-icon>add</v-icon>
             <span>ایجاد جدید</span>
           </v-btn>
