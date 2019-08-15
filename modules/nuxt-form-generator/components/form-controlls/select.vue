7189da8c63998d8b9fe7b95697f28f75dd1841ad
nmomenyan <momenyan@gmail.com>
2019-08-15 17:17:19 +0430

    v-select dynamic text,value

--- modules/nuxt-form-generator/components/form-controlls/select.vue
+++ modules/nuxt-form-generator/components/form-controlls/select.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="form-group">
     <label v-if="!minimal">{{field.label}}</label>
-    <v-select :error-messages="errorMessages" v-bind="$attrs" v-model="model"></v-select>
+    <template v-if="field.select_text">
+      <v-select
+        :error-messages="errorMessages"
+        v-bind="$attrs"
+        v-model="model"
+        :item-text="field.select_text"
+        :item-value="field.select_value"
+      ></v-select>
+    </template>
+    <template v-else>
+      <v-select :error-messages="errorMessages" v-bind="$attrs" v-model="model"></v-select>
+    </template>
   </div>
 </template>
 <script lang="ts">
