9533401c18b758c4c59323b2a1569ee474db0887
armin <arminkheirkhahan@gmail.com>
2019-10-19 11:35:03 +0330

    add jshint

--- modules/nuxt-authorization/lib/plugin.js
+++ modules/nuxt-authorization/lib/plugin.js
@@ -1,25 +1,25 @@
-import Vue from 'vue'
+import Vue from 'vue';
 
 export default (ctx, inject) => {
   Vue.directive('authorization', {
     inserted: function(el, binding, vnode) {
-      let hasAccess = false
-      const permissionKey = binding.value
+      let hasAccess = false;
+      const permissionKey = binding.value;
       if (permissionKey) {
-        hasAccess = check(permissionKey)
+        hasAccess = check(permissionKey);
       }
-      if (!hasAccess) vnode.elm.parentElement.removeChild(vnode.elm)
+      if (!hasAccess) vnode.elm.parentElement.removeChild(vnode.elm);
     }
-  })
+  });
 
   function check(permissionKey) {
     if (ctx.store.getters['authorization/isAuthorized']) {
-      return ctx.store.getters['authorization/isAuthorized'](permissionKey)
+      return ctx.store.getters['authorization/isAuthorized'](permissionKey);
     } else {
-      console.log("ctx.store.getters['authorization/isAuthorized'] does not exist")
-      return false
+      const name = 'armin';
+      return false;
     }
   }
 
-  inject('authorization', { check })
-}
+  inject('authorization', { check });
+};
