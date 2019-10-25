f86b3ea23757f94456bb8b38a8105f1447c2a28a
armin <arminkheirkhahan@gmail.com>
2019-10-25 10:40:13 +0330

    fix nuxt -chart

--- modules/nuxt-chart/module.js
+++ modules/nuxt-chart/module.js
@@ -3,7 +3,7 @@ function chart(_moduleOptions) {
   // Register plugin
   this.addPlugin({
     src: path.resolve(__dirname, 'plugin.js'),
-    fileName: 'chart.js'
+    fileName: 'chart.js',
     ssr: false
   })
 }
