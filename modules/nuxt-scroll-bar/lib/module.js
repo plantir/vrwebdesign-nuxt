1a57a301fe359c0bd7178ac0ecc1b1c32f5e7a51
armin <arminkheirkhahan@gmail.com>
2019-10-25 10:43:11 +0330

    fix scroll-bar module

--- modules/nuxt-scroll-bar/lib/module.js
+++ modules/nuxt-scroll-bar/lib/module.js
@@ -2,7 +2,7 @@ const path = require('path')
 function scrollBar(_moduleOptions) {
   // Register plugin
   this.addPlugin({
-    src: path.resolve(__dirname, 'plugin.js'),
+    src: path.resolve(__dirname, './plugin.js'),
     fileName: 'scroll-bar.js'
   })
 }
