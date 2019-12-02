5f79735b8a01341cfe184354af4a07807c9b1a30
armin <arminkheirkhahan@gmail.com>
2019-12-02 15:25:12 +0330

    remove extra interface

--- modules/nuxt-chart/types/index.d.ts
+++ modules/nuxt-chart/types/index.d.ts
@@ -1,26 +1,3 @@
-export interface IChart {
-  type: string
-  title: string
-  subTitle?: string
-  xAxisCategories: string[]
-  yTitle?: string
-  dataSeries: IDataSeries[]
-}
-
-export interface IPieChart {
-  title: string
-  dataSeries: IPieDataSeries[]
-}
-
-export interface IDataSeries {
-  name: string
-  data: (number | string)[]
-}
-
-export interface IPieDataSeries {
-  name: string
-  amount: number
-}
 export interface VRChartOption {
   yAxis_title?: String
   start_date?: String
