5bd8a85eded168c0d3d66182cdd06c384402d5eb
armin <arminkheirkhahan@gmail.com>
2020-02-11 11:06:03 +0330

    change scroll to type

--- modules/nuxt-scroll-to/types/index.d.ts
+++ modules/nuxt-scroll-to/types/index.d.ts
@@ -1,49 +1,34 @@
 import Vue from 'vue'
-interface NuxtScrollToObject {
-  element: Vue | Element | Vue[] | Element[] | String
-  duration: number
-  option: {
-    container: Vue | Element | Vue[] | Element[] | String
-    offset: number
-  }
+interface NuxtScrollTo {
+  (
+    element: Vue | Element | Vue[] | Element[] | String,
+    duration?: number,
+    option?: {
+      container?: Vue | Element | Vue[] | Element[] | String
+      offset?: number
+    }
+  ): void
 }
 
-declare module '@nuxt/vue-app' {
+declare module '@nuxt/types' {
   interface Context {
-    $scrollTo: (
-      element: Vue | Element | Vue[] | Element[] | String,
-      duration?: number,
-      option?: {
-        container?: Vue | Element | Vue[] | Element[] | String
-        offset?: number
-      }
-    ) => void
+    $scrollTo: NuxtScrollTo
   }
 }
 
 // since nuxt 2.7.1 there is "NuxtAppOptions" for app context - see https://github.com/nuxt/nuxt.js/pull/5701
-declare module '@nuxt/vue-app' {
+declare module '@nuxt/types' {
   interface NuxtAppOptions {
-    $scrollTo: (
-      element: Vue | Element | Vue[] | Element[] | String,
-      duration?: number,
-      option?: {
-        container?: Vue | Element | Vue[] | Element[] | String
-        offset?: number
-      }
-    ) => void
+    $scrollTo: NuxtScrollTo
   }
 }
-
-// declare module 'vue/types/vue' {
-//   interface Vue {
-//     $scrollTo: (
-//       element: Vue | Element | Vue[] | Element[] | String,
-//       duration?: number,
-//       option?: {
-//         container?: Vue | Element | Vue[] | Element[] | String
-//         offset?: number
-//       }
-//     ) => void
-//   }
-// }
+declare module 'vuex/types/index' {
+  interface Store<S> {
+    $scrollTo: NuxtScrollTo
+  }
+}
+declare module 'vue/types/vue' {
+  interface Vue {
+    $scrollTo: NuxtScrollTo
+  }
+}
