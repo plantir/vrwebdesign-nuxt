a4e800aa13a3c0eb8f2f3f8206665d331bfe26ca
armin <arminkheirkhahan@gmail.com>
2020-04-14 09:28:17 +0430

    fix datepicker format

--- modules/nuxt-form-generator/components/form-controlls/datePicker.vue
+++ modules/nuxt-form-generator/components/form-controlls/datePicker.vue
@@ -7,6 +7,7 @@
       v-model="model"
       :auto-submit="true"
       :type="field.datePickerType || 'date'"
+      :format="format"
     ></vr-date-picker>
   </div>
 </template>
@@ -14,6 +15,31 @@
 import Vue from 'vue'
 import mixins from './mixins'
 export default Vue.extend({
-  mixins: [mixins]
+  mixins: [mixins],
+  computed: {
+    format() {
+      debugger
+      switch (this.field.datePickerType) {
+        case 'datetime':
+          return 'jYYYY/jMM/jDD HH:mm'
+          break
+        case 'time':
+          return 'HH:mm'
+          break
+        case 'year':
+          return 'jYYYY'
+          break
+        case 'month':
+          return 'jMM'
+          break
+        case 'year-month':
+          return 'jYYYY/jMM'
+          break
+        default:
+          return 'jYYYY/jMM/jDD'
+          break
+      }
+    }
+  }
 })
 </script>
