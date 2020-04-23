8da2e3a9f5a2fcceedc0c22cb9d6abd569202acb
armin <arminkheirkhahan@gmail.com>
2020-04-24 03:25:12 +0430

    add filter to enums

--- modules/nuxt-enums/lib/module.js
+++ modules/nuxt-enums/lib/module.js
@@ -10,19 +10,21 @@ function axiosModule(_moduleOptions) {
   var enum_path = path.join(this.options.rootDir, 'enums')
   if (fs.existsSync(enum_path)) {
     // Do something
-    fs.readdirSync(enum_path).forEach(function(file) {
+    fs.readdirSync(enum_path).forEach(function (file) {
       if (file.includes('.ts') || file.includes('.js')) {
         var file_path = path.join(enum_path, file)
         delete require.cache[require.resolve(file_path)]
         //  to have this service in enums
         const file_name = file.replace(/\.(js|ts)/, '')
         enums[file_name] = _interopDefault(require(file_path))
-        let convertToSelect = Object.keys(enums[file_name]).map(key => {
-          return {
-            text: enums[file_name][key],
-            value: isNaN(key) ? key : parseInt(key)
-          }
-        })
+        let convertToSelect = Object.keys(enums[file_name])
+          .filter((key) => !isNaN(key))
+          .map((key) => {
+            return {
+              text: enums[file_name][key],
+              value: isNaN(key) ? key : parseInt(key),
+            }
+          })
         enums[file_name]['toSelect'] = convertToSelect
       }
     })
@@ -33,7 +35,7 @@ function axiosModule(_moduleOptions) {
     ssr: false,
     src: path.resolve(__dirname, 'plugin.js'),
     fileName: 'enums.js',
-    options: this.options
+    options: this.options,
   })
 }
 
