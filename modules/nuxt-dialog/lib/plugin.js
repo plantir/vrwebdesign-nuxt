194f39c1f7012abc6e85287fd4bfddc16360dbb4
armin <arminkheirkhahan@gmail.com>
2021-04-24 20:57:18 +0430

    add dialog in ctx

--- modules/nuxt-dialog/lib/plugin.js
+++ modules/nuxt-dialog/lib/plugin.js
@@ -3,6 +3,7 @@ import Dialog from 'vrwebdesign-nuxt/modules/nuxt-dialog/plugin/index'
 
 Vue.use(Dialog)
 
-export default function(ctx, inject) {
+export default function (ctx, inject) {
   inject('dialog', Dialog)
+  ctx.$dialog = Dialog
 }
